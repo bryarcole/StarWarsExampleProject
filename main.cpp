67ca48848b61a03510a18540f7a6d120414c76e6
bryarcole <bryarcole@gmail.com>
2015-10-11 19:15:07 -0500

    Function version of Lab3
    Did lab3 using functions like the example shown in class.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char arg[]) {
+    string firstName,
+        secondName,
+        thirdName,
+        homeTownName;
+
+    cout << "Welcome to the Stars Wars name Generator!";
+    cout << "What is your first name ";
+    cin >> firstName;
+    cout <<
+    return 0;
+}
